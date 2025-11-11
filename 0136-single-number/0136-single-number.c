f05947db3093589102ed8b4e221cced5bc7ea934
mahreenqaiser123 <mahreenqaiser000@gmail.com>
2025-11-11 22:23:27 +0500

    Time: 0 ms (100%), Space: 9.3 MB (97.56%) - LeetHub

---
+++ 0136-single-number/0136-single-number.c
@@ -0,0 +1,7 @@
+int singleNumber(int* nums, int numsSize) {
+    int result = 0;
+    for (int i = 0; i < numsSize; i++) {
+        result ^= nums[i]; // XOR cancels out pairs
+    }
+    return result;
+}
